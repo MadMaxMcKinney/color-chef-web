7966b8cafa8425706c423c0bbfc469c82e6f4f54
Max McKinney <maxgames101@gmail.com>
2023-10-01 17:39:29 -0500

    seo: add title and basic meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import '$lib/app.css';
 </script>
 
+<svelte:head>
+    <title>Color Chef</title>
+    <meta name="description" content="Generate a color palette by describing how it will be used. The Chef will do the rest, sick back and enjoy!" />
+</svelte:head>
+
 <slot />
